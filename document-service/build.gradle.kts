fb8281dc2662296b0b62ae12ecd62d3ca3571cb9
Amit Pal <amix.pal@gmail.com>
2024-08-09 00:14:17 +0200

    Modifying Docker files for both service to make them faster
    - using parallel and removing no-deamon
    - docker-compose modificaton as per the Docker files requirement

--- document-service/build.gradle.kts
+++ document-service/build.gradle.kts
@@ -1,53 +1,40 @@
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-}
 plugins {
     java
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
 }
 
-allprojects {
-    group = "com.example"
-    version = "0.0.1-SNAPSHOT"
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
 
-    repositories {
-        mavenCentral()
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
-subprojects {
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "java")
-
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
-        }
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
     }
+}
 
-    configurations {
-        compileOnly {
-            extendsFrom(configurations.annotationProcessor.get())
-        }
-    }
-
-    dependencies {
-        implementation(project(":common"))
-        implementation("org.springframework.boot:spring-boot-starter")
-        implementation("org.springframework.boot:spring-boot-starter-web")
-        implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-        implementation("org.springframework.boot:spring-boot-starter-validation")
-        compileOnly("org.projectlombok:lombok")
-        developmentOnly("org.springframework.boot:spring-boot-devtools")
-        annotationProcessor("org.projectlombok:lombok")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
